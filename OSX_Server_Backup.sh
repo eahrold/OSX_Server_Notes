549f8a47a10fe31eae24d431b0d0db982f395988
Eldon Ahrold <eahrold@loyno.edu>
2014-04-18 07:51:28 -0500

    / ‘OSX_Server_Backup.sh’

--- OSX_Server_Backup.sh
+++ OSX_Server_Backup.sh
@@ -47,7 +47,7 @@ backup_postgres(){
 
 backup_ldap(){
 	# slapconfig -backupdb
-	serveradmin command dirserv:backupArchiveParams:archivePassword = $OD_ARCHIVE_PASSWORD
+	serveradmin command dirserv:backupArchiveParams:archivePassword = "$OD_ARCHIVE_PASSWORD"
 	serveradmin command dirserv:backupArchiveParams:archivePath = "${FINAL_DEST}"/ODArchive.dmg
 	serveradmin command dirserv:command = backupArchive
 }
